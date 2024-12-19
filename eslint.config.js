3f3e4b9c47d2d63b80ea0c3b81203e4eb11be058
Dmitriy_Zakirov <metrio@inbox.ru>
2024-12-19 16:45:52 +0500

    ESLint and StyleLint configured

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,29 @@
 import globals from "globals";
 
 const customRules = {
-  "array-bracket-spacing": [2, "always"],
-  "quotes": ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
+  "array-bracket-spacing": [ 2, "always" ],
+  "quotes": [ "error", "double", { "avoidEscape": true, "allowTemplateLiterals": true } ],
   "no-console": [
     "error",
-    { "allow": ["warn", "error", "debug", "asset", "table", "time", "timeEnd"] },
+    { "allow": [ "warn", "error", "debug", "asset", "table", "time", "timeEnd" ] },
   ],
-  "no-unused-vars": ["error", { "args": "none" }],
+  "no-unused-vars": [ "error", { "args": "none" } ],
   "curly": "error",
-  "object-curly-spacing": ["error", "always"],
+  "object-curly-spacing": [ "error", "always" ],
   "no-redeclare": 2,
   "no-shadow-restricted-names": 2,
-  "eqeqeq": ["error", "always"],
+  "eqeqeq": [ "error", "always" ],
 };
 
-const getIgnoreFolders = (folders = ["node_modules", "build", "public", "assets", "dist", "temp", ".temp", ".cache", "cache"]) => {
+const getIgnoreFolders = (folders = [ "node_modules", "build", "public", "assets", "dist", "temp", ".temp", ".cache", "cache" ]) => {
   const maskName = "%mask%";
-  const ignoreMasks = [`**/${maskName}/`, `/**/${maskName}/*`, `${maskName}/`];
+  const ignoreMasks = [ `**/${maskName}/`, `/**/${maskName}/*`, `${maskName}/` ];
   return folders
       .map((folder) => ignoreMasks.map(item => item.replace(maskName, folder)))
       .flat();
 };
 
-const files = ["js", "jsx", "mjs"].map(ex => `**/*.${ex}`);
+const files = [ "js", "jsx", "mjs" ].map(ex => `**/*.${ex}`);
 
 const languageOptions = {
   "globals": {
